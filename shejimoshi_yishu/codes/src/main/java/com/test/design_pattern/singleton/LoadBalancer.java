5bebc8ff329e7a4c6b694c7604bcd9260959617e
CodingWorker <dy10010@sina.cn>
2017-09-28 09:27:43 +0800

    add

---
+++ shejimoshi_yishu/codes/src/main/java/com/test/design_pattern/singleton/LoadBalancer.java
@@ -0,0 +1,59 @@
+package com.test.design_pattern.singleton;
+
+/**
+ * Created by IntelliJ IDEA
+ * Project: NotesDemo
+ * User: DaiYan
+ * Date: 2017/9/28
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * 单例模式的实例：
+ *      负载均衡器的实现
+ */
+public class LoadBalancer {
+    private static LoadBalancer instance=new LoadBalancer();
+
+    /**
+     * 服务器列表
+     */
+    private List<String> serveList=null;
+
+    private LoadBalancer(){
+        serveList=new ArrayList<String>();
+    }
+
+    public static LoadBalancer GetInstance(){
+        return instance;
+    }
+
+    /**
+     * add server
+     */
+    public void addServer(String server){
+        this.serveList.add(server);
+    }
+
+    /**
+     * remove server
+     */
+    public void removeServer(String server){
+        this.serveList.remove(server);
+    }
+
+    /**
+     * 负载均衡：随机获取一个服务器
+     * 负载均衡：随机获取一个服务器
+     */
+    public String getRandom(){
+        int size=this.serveList.size();
+        if(0==this.serveList.size()) return null;
+
+        int index=new Random().nextInt()*size;
+        return this.serveList.get(index);
+    }
+}
---
+++ shejimoshi_yishu/codes/src/main/java/com/test/design_pattern/singleton/Singleton.java
@@ -0,0 +1,52 @@
+package com.test.design_pattern.singleton;
+
+/**
+ * Created by IntelliJ IDEA
+ * Project: NotesDemo
+ * User: DaiYan
+ * Date: 2017/9/28
+ */
+
+/**
+ * 设计步骤：
+ *      1. 私有化构造器
+ *      2. 私有的该类的静态成员变量
+ *      3. 公共的外部访问该成员变量的静态方法
+ *
+ *  两种构造方式：
+ *      1. 懒汉式
+ *      2. 饿汉式
+ */
+
+/**
+ * 懒汉式
+ */
+public class Singleton {
+    private static Singleton instance;
+
+    private Singleton(){}
+
+    public static Singleton getInstance(){
+        if(null==instance){
+            synchronized (Singleton.class){
+                if(null==instance){
+                    instance=new Singleton();
+                }
+            }
+        }
+
+        return instance;
+    }
+}
+
+/**
+ * 饿汉式
+ */
+class Singleton2{
+    private static Singleton2 instance=new Singleton2();
+    private Singleton2(){}
+
+    public static Singleton2 getInstance(){
+        return instance;
+    }
+}
